69b50c7e8fd28b7daf031fcad93e2593948d22c0
yuki <shintarou.benkyouyou@gmail.com>
2021-03-20 11:56:53 +0000

    @tasks = Task.allに変更しました

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]  
   def index
-    @task = Task.all
+    @tasks = Task.all
   end
   
   def show
